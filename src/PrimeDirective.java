dc604b5b34c01553ced46514d2cd0bed320e15b0
satinder <satindersingh472@gmail.com>
2023-01-01 13:33:02 -0700

    commenting_done

--- src/PrimeDirective.java
+++ src/PrimeDirective.java
@@ -1,7 +1,11 @@
+//import java arraylist
 import java.util.ArrayList;
 
+
+//class is PrimeDirective
 class PrimeDirective {
 
+//    method will return true if number is prime
     public boolean isPrime(int number){
         for(int i = 2; i < number; i++){
             if(number % i == 0){
@@ -10,6 +14,11 @@ class PrimeDirective {
         }
         return true;
     }
+//    this method will create an arraylist
+//    and accept the array as a parameter and run for each loop on that
+//    will call the isPrime method and check for the boolean value
+//    if returned value is true then add the integer into an arraylist
+//    return the arraylist
     public ArrayList<Integer> onlyPrimes(int[] numbers){
         ArrayList<Integer> primes = new ArrayList<Integer>();
         for(int number : numbers){
